bf612291d000b2ae003a139a22a9b6367a0edf72
Eirik Nerdal <eirik.nerdal@gmail.com>
2026-04-13 16:24:32 +0200

    Bind express to 0.0.0.0 explicitly to fix 502 bad gateway

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use((err, req, res, next) => {
 })
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`))
 
 if (process.env.NODE_ENV !== 'test') {
   require('./jobs/scheduler')
